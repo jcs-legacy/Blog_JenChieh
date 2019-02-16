2de8da4030a51bf6b44b7a4a269837698b645c7c
jenchieh <jcs090218@gmail.com>
2019-02-17 01:22:29 +0800

    Add valid symbol conversion.

--- website/js/main.js
+++ website/js/main.js
@@ -44,8 +44,28 @@
   var slashKey = "_sl_";
   var spaceKey = "_sp_";
 
-  var obKey = "_ob_";  // oepn bracket
-  var cbKey = "_cb_";  // close bracket
+  var obKey      = "_ob_";   // open bracket '('.
+  var cbKey      = "_cb_";   // close bracket ')'.
+  var ocbKey     = "_ocb_";  // open curly bracket '{'.
+  var ccbKey     = "_ccb_";  // close curly bracket '}'.
+  var osbKey     = "_osb_";  // open square bracket '['.
+  var csbKey     = "_csb_";  // close square bracket ']'.
+
+  var atKey      = "_at_";   // At key '@'.
+  var caretKey   = "_cr_";   // Caret key '^'.
+  var bqKey      = "_bq_";   // Back quote key '`'.
+  var tildeKey   = "_tl_";   // Tilde key '~'.
+  var hashKey    = "_hs_";   // Hash key '#'.
+  var dollarKey  = "_dl_";   // Dollar '$' key.
+  var percentKey = "_pc_";   // Percent '%' key.
+  var andKey     = "_and_";  // And '&' key.
+  var plusKey    = "_pl_";   // And '+' key.
+  var quoteKey   = "_qt_";   // Quote ' key.
+
+  var periodKey  = "_pri_";  // Period '.' key.
+  var eqKey      = "_eq_";   // Equals ' =' key.
+  var cmaKey     = "_cma_";  // Comma ',' key.
+  var scKey      = "_sc_";   // Semicolon ';' key.
 
 
   //---------------------- Functions ---------------------------//
@@ -563,13 +583,55 @@
     if (revert) {
       rawStr = rawStr.split(slashKey).join("/");
       rawStr = rawStr.split(spaceKey).join(" ");
+
       rawStr = rawStr.split(obKey).join("(");
       rawStr = rawStr.split(cbKey).join(")");
+      rawStr = rawStr.split(ocbKey).join("{");
+      rawStr = rawStr.split(ccbKey).join("}");
+      rawStr = rawStr.split(osbKey).join("[");
+      rawStr = rawStr.split(csbKey).join("]");
+
+      rawStr = rawStr.split(atKey).join("@");
+      rawStr = rawStr.split(bqKey).join("`");
+      rawStr = rawStr.split(caretKey).join("^");
+      rawStr = rawStr.split(tildeKey).join("~");
+      rawStr = rawStr.split(hashKey).join("#");
+      rawStr = rawStr.split(dollarKey).join("$");
+      rawStr = rawStr.split(percentKey).join("%");
+      rawStr = rawStr.split(andKey).join("&");
+      rawStr = rawStr.split(plusKey).join("+");
+      rawStr = rawStr.split(quoteKey).join("'");
+
+      rawStr = rawStr.split(periodKey).join(".");
+      rawStr = rawStr.split(eqKey).join("=");
+      rawStr = rawStr.split(cmaKey).join(",");
+      rawStr = rawStr.split(scKey).join(";");
     } else {
       rawStr = rawStr.replace(/\//g, slashKey);
       rawStr = rawStr.replace(/ /g, spaceKey);
+
       rawStr = rawStr.replace(/\(/g, obKey);
       rawStr = rawStr.replace(/\)/g, cbKey);
+      rawStr = rawStr.replace(/\{/g, ocbKey);
+      rawStr = rawStr.replace(/\}/g, ccbKey);
+      rawStr = rawStr.replace(/\[/g, osbKey);
+      rawStr = rawStr.replace(/\]/g, csbKey);
+
+      rawStr = rawStr.replace(/\@/g, atKey);
+      rawStr = rawStr.replace(/\`/g, bqKey);
+      rawStr = rawStr.replace(/\^/g, caretKey);
+      rawStr = rawStr.replace(/\~/g, tildeKey);
+      rawStr = rawStr.replace(/\#/g, hashKey);
+      rawStr = rawStr.replace(/\$/g, dollarKey);
+      rawStr = rawStr.replace(/\%/g, percentKey);
+      rawStr = rawStr.replace(/\&/g, andKey);
+      rawStr = rawStr.replace(/\+/g, plusKey);
+      rawStr = rawStr.replace(/\'/g, quoteKey);
+
+      rawStr = rawStr.replace(/\./g, periodKey);
+      rawStr = rawStr.replace(/\=/g, eqKey);
+      rawStr = rawStr.replace(/\,/g, cmaKey);
+      rawStr = rawStr.replace(/\;/g, scKey);
     }
     return rawStr;
   }
